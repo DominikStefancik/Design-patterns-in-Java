5ce43564efa73140c8759c21aaa4be6dadfe884e
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-11-15 09:35:02 +0100

    Remove setters to make DTO objects immutable

--- src/main/java/com/nortoncommander/designpatterns/creational/builder/version1/impl/rest/UserRestDTO.java
+++ src/main/java/com/nortoncommander/designpatterns/creational/builder/version1/impl/rest/UserRestDTO.java
@@ -22,32 +22,16 @@ public class UserRestDTO implements UserDTO {
     return firstName;
   }
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
   public String getLastName() {
     return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
   }
 
   public String getBirthday() {
     return birthday;
   }
 
-  public void setBirthday(String birthday) {
-    this.birthday = birthday;
-  }
-
   public String getAddress() {
     return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
   }
 
   @Override
--- src/main/java/com/nortoncommander/designpatterns/creational/builder/version1/impl/web/UserWebDTO.java
+++ src/main/java/com/nortoncommander/designpatterns/creational/builder/version1/impl/web/UserWebDTO.java
@@ -20,24 +20,12 @@ public class UserWebDTO implements UserDTO {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public String getAddress() {
     return address;
   }
 
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
   public String getAge() {
     return age;
-  }
-
-  public void setAge(String age) {
-    this.age = age;
   }
 
   @Override
